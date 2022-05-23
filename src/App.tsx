52224aee608439f5493ac3fd692c52819b80def1
Dmitriy24S <devilins24@gmail.com>
2022-05-23 06:10:53 +0300

    feat: :sparkles: pull data from mockAPI, separate card component for data item, add/remove to/from cart func

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,29 @@ import Slider from "./components/Slider/Slider";
 
 function App() {
   const [isCartDrawerOpen, setIsCartDrawerOpen] = useState<boolean>(false);
+  const [items, setItems] = useState([]);
+  const [cartItems, setCartItems] = useState<any[]>([]);
+
+  const handleAddToCart = (item: any) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  const handleDeleteFromCart = (id: any) => {
+    const updatedCart = cartItems.filter((item) => item.id !== id);
+    setCartItems(updatedCart);
+  };
+
+  // fetch data
+  useEffect(() => {
+    fetch("https://628a5b65e5e5a9ad3223b0a7.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        console.log(json);
+        setItems(json);
+      });
+  }, []);
 
   // disable main page scroll when cart drawer is open
   useEffect(() => {
@@ -18,10 +41,20 @@ function App() {
 
   return (
     <div className="App">
-      {isCartDrawerOpen && <Drawer setIsCartDrawerOpen={setIsCartDrawerOpen} />}
+      {isCartDrawerOpen && (
+        <Drawer
+          setIsCartDrawerOpen={setIsCartDrawerOpen}
+          cartItems={cartItems}
+          handleDeleteFromCart={handleDeleteFromCart}
+        />
+      )}
       <Header setIsCartDrawerOpen={setIsCartDrawerOpen} />
       <Slider />
-      <Main />
+      <Main
+        items={items}
+        handleAddToCart={handleAddToCart}
+        handleDeleteFromCart={handleDeleteFromCart}
+      />
     </div>
   );
 }
--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -3,9 +3,15 @@ import "./Drawer.scss";
 
 type DrawerProps = {
   setIsCartDrawerOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  cartItems: any;
+  handleDeleteFromCart: any;
 };
 
-const Drawer = ({ setIsCartDrawerOpen }: DrawerProps) => {
+const Drawer = ({
+  setIsCartDrawerOpen,
+  cartItems,
+  handleDeleteFromCart,
+}: DrawerProps) => {
   return (
     <section className="drawer">
       <div
@@ -45,127 +51,98 @@ const Drawer = ({ setIsCartDrawerOpen }: DrawerProps) => {
           </button>
         </div>
         {/* cart content list */}
-        <section className="cart-content">
-          {/* item 1 */}
-          <article className="cart-item">
-            {/* info */}
-            <img
-              src={`${process.env.PUBLIC_URL}/img/sneakers/1.jpg`}
-              alt="sneaker"
-              className="item__image"
-            />
-            <div className="cart-item__info">
-              <div className="item__name">
-                <span>Men Sneakers</span>
-                <h5>Nike Blazer Mid Suede</h5>
+        {cartItems.length ? (
+          <>
+            <section className="cart-content">
+              {cartItems.map((item: any, index: number) => {
+                return (
+                  <article className="cart-item">
+                    {/* info */}
+                    <img
+                      src={`${process.env.PUBLIC_URL}${item.imageUrl}`}
+                      alt="sneaker"
+                      className="item__image"
+                    />
+                    <div className="cart-item__info">
+                      <div className="item__name">
+                        <span>Men Sneakers</span>
+                        <h5>{item.title}</h5>
+                      </div>
+                      <div className="item__price">
+                        <p>£{item.price}</p>
+                      </div>
+                    </div>
+                    <button
+                      aria-label="remove from cart"
+                      className="remove-cart-btn"
+                      onClick={() => handleDeleteFromCart(item.id)}
+                    >
+                      <svg
+                        width="32"
+                        height="32"
+                        viewBox="0 0 32 32"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <rect
+                          x="0.5"
+                          y="0.5"
+                          width="31"
+                          height="31"
+                          rx="7.5"
+                          fill="white"
+                          stroke="#DBDBDB"
+                        />
+                        <path
+                          d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z"
+                          fill="#B5B5B5"
+                        />
+                      </svg>
+                    </button>
+                  </article>
+                );
+              })}
+            </section>
+            {/* cart bottom */}
+            <section className="cart-bottom">
+              <div className="cart-total">
+                Total: <div className="cart-total-divider"></div>
+                <span className="cart-total-price">£199.99</span>
               </div>
-              <div className="item__price">
-                {/* <span>Price:</span> */}
-                <p>£45.99</p>
-              </div>
-            </div>
-            <button aria-label="remove from cart" className="remove-cart-btn">
-              <svg
-                width="32"
-                height="32"
-                viewBox="0 0 32 32"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  x="0.5"
-                  y="0.5"
-                  width="31"
-                  height="31"
-                  rx="7.5"
-                  fill="white"
-                  stroke="#DBDBDB"
-                />
-                <path
-                  d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z"
-                  fill="#B5B5B5"
-                />
-              </svg>
-            </button>
+              <button className="place-order-btn">
+                Place order
+                {/* order btn - arrow svg */}
+                <svg
+                  className="place-order-btn-arrow"
+                  width="16"
+                  height="14"
+                  viewBox="0 0 16 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1 7H14.7143"
+                    stroke="white"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M8.71436 1L14.7144 7L8.71436 13"
+                    stroke="white"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </button>
+            </section>
+          </>
+        ) : (
+          <article className="empty-cart-message">
+            <h2>Cart is empty</h2>
           </article>
-          {/* item 2 */}
-          <article className="cart-item">
-            {/* info */}
-            <img
-              src={`${process.env.PUBLIC_URL}/img/sneakers/2.jpg`}
-              alt="sneaker"
-              className="item__image"
-            />
-            <div className="cart-item__info">
-              <div className="item__name">
-                <span>Men Sneakers</span>
-                <h5>Nike Blazer Mid Suede</h5>
-              </div>
-              <div className="item__price">
-                {/* <span>Price:</span> */}
-                {/* <span>£45.99</span> */}
-                <p>£45.99</p>
-              </div>
-            </div>
-            <button aria-label="remove from cart" className="remove-cart-btn">
-              <svg
-                width="32"
-                height="32"
-                viewBox="0 0 32 32"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  x="0.5"
-                  y="0.5"
-                  width="31"
-                  height="31"
-                  rx="7.5"
-                  fill="white"
-                  stroke="#DBDBDB"
-                />
-                <path
-                  d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z"
-                  fill="#B5B5B5"
-                />
-              </svg>
-            </button>
-          </article>
-        </section>
-        {/* cart bottom */}
-        <section className="cart-bottom">
-          <div className="cart-total">
-            Total: <div className="cart-total-divider"></div>
-            <span className="cart-total-price">£199.99</span>
-          </div>
-          <button className="place-order-btn">
-            Place order
-            {/* order btn - arrow svg */}
-            <svg
-              className="place-order-btn-arrow"
-              width="16"
-              height="14"
-              viewBox="0 0 16 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M1 7H14.7143"
-                stroke="white"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M8.71436 1L14.7144 7L8.71436 13"
-                stroke="white"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
-        </section>
+        )}
       </div>
     </section>
   );
