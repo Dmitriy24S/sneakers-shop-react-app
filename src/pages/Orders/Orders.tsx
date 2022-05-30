b41cacda1abe5fdc62016f322a9c4c1384eaf3c9
Dmitriy24S <devilins24@gmail.com>
2022-05-30 14:40:43 +0300

    feat: :sparkles: add loading spinner to orders page, loading state on data fetch

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { SpinnerCircularFixed } from "spinners-react";
 import "./Orders.scss";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
+  //   const [orders, setOrders] = useState<[] | null>(null);
+  const [isLoadingData, setIsLoadingData] = useState(true);
 
   // Fetch order list on page load
   useEffect(() => {
@@ -17,7 +20,9 @@ const Orders = () => {
         alert("Error fetching orders");
         console.log(error);
       }
+      setIsLoadingData(false);
     };
+
     fetchOrders();
   }, []);
 
@@ -28,48 +33,63 @@ const Orders = () => {
           <h1 className="section-title">Your Orders</h1>
         </div>
         <section className="items-container order-list-container">
-          {/* No orders message */}
-          {orders.length === 0 && (
-            <article className="empty-data-message">
-              <h2>No data to display</h2>
+          {isLoadingData ? (
+            // Loading spinner
+            <article className="empty-data-message loading-spinner-container">
+              <SpinnerCircularFixed
+                size={62}
+                thickness={113}
+                speed={100}
+                color="rgba(86, 212, 238, 1)"
+                secondaryColor="rgba(57, 118, 172, 0.12)"
+              />
             </article>
+          ) : (
+            <>
+              {/* No orders message */}
+              {orders.length === 0 && (
+                <article className="empty-data-message">
+                  <h2>No data to display</h2>
+                </article>
+              )}
+              {/* List orders */}
+              {orders?.map((item: any) => {
+                let date = new Date(item.createdAt);
+                let dayDate = date.toDateString();
+                return (
+                  <article className="order" key={item.id}>
+                    <div className="order-header">
+                      <h4 className="order-number">Order #{item.id}</h4>
+                      <p className="order-price">£149.99</p>
+                    </div>
+                    <p className="order-date">{dayDate}</p>
+                    {/* products in order preview */}
+                    <div className="order-preview-img">
+                      {item?.order?.map((item: any, index: number) => {
+                        if (index < 3) {
+                          return (
+                            <div className="preview-img--wrapper" key={item.id}>
+                              <img
+                                src={`${process.env.PUBLIC_URL}${item.imageUrl}`}
+                                alt="sneaker"
+                              />
+                            </div>
+                          );
+                        }
+                        return null;
+                      })}
+                      {/* after first 3 preview images show '...' instead of all products images */}
+                      {item?.order?.length > 2 && (
+                        <span className="extra-items-preview">...</span>
+                      )}
+                    </div>
+                    {/* total products in order */}
+                    <p className="orders-amount">{item.order.length} items</p>
+                  </article>
+                );
+              })}
+            </>
           )}
-          {/* List orders */}
-          {orders?.map((item: any) => {
-            let date = new Date(item.createdAt);
-            let dayDate = date.toDateString();
-            return (
-              <article className="order" key={item.id}>
-                <div className="order-header">
-                  <h4 className="order-number">Order #{item.id}</h4>
-                  <p className="order-price">£149.99</p>
-                </div>
-                <p className="order-date">{dayDate}</p>
-                {/* products in order preview */}
-                <div className="order-preview-img">
-                  {item?.order?.map((item: any, index: number) => {
-                    if (index < 3) {
-                      return (
-                        <div className="preview-img--wrapper" key={item.id}>
-                          <img
-                            src={`${process.env.PUBLIC_URL}${item.imageUrl}`}
-                            alt="sneaker"
-                          />
-                        </div>
-                      );
-                    }
-                    return null;
-                  })}
-                  {/* after first 3 preview images show '...' instead of all products images */}
-                  {item?.order?.length > 2 && (
-                    <span className="extra-items-preview">...</span>
-                  )}
-                </div>
-                {/* total products in order */}
-                <p className="orders-amount">{item.order.length} items</p>
-              </article>
-            );
-          })}
         </section>
       </div>
     </main>
